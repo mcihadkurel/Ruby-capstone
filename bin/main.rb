c44cae3d3a3441a78c690046febff8bb82564374
Muhammed Cihad Kurel <65771498+mcihadkurel@users.noreply.github.com>
2020-09-18 20:21:04 +0300

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,13 @@
 
+# !/usr/bin/env ruby
+
+require 'csv'
+require_relative '../lib/scraper.rb'
+
+private
+
+def hostel_card
+  parsed_page.css('li.job.inline.featured')
+end
+
+Scraper.new('https://cryptojobslist.com/', @unparse, @parsed_page).scrape
