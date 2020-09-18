3d071823c4d8ebc227171c05d8090437272cfc56
Muhammed Cihad Kurel <65771498+mcihadkurel@users.noreply.github.com>
2020-09-18 20:23:31 +0300

    Add rspec test

--- spec/spec_test.rb
+++ spec/spec_test.rb
@@ -1,1 +1,12 @@
 
+require 'spec_helper'
+require_relative '../bin/main.rb'
+
+describe Scraper do
+  describe '#scrape' do
+    let(:scrap) { Scraper.new('https://cryptojobslist.com/', @doc, @document) }
+    it 'when getting the joblist' do
+      expect(scrap.scrape).to equal(30)
+    end
+  end
+end
